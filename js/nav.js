381ce49095837e6605af85fc47d9329245c2bd9a
nasser-gamal <98104328+nasser-gamal@users.noreply.github.com>
2022-07-04 13:04:24 +0200

    Add Some Changes

--- js/nav.js
+++ js/nav.js
@@ -4,3 +4,17 @@ nav = document.querySelector(".header .container .links")
 btn.onclick = () => {
     nav.classList.toggle("show");
 };
+
+
+// Hide Loading
+
+const loading = document.querySelector(".loading")
+
+function hide() {
+    setTimeout(() => {
+        loading.classList.add("hidden")
+    }, 1000);
+}
+
+
+hide()
